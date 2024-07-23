a94779ded7cfd6eccb7f4e8c63974cd886cc675d
anant2961 <anant2961@gmail.com>
2024-07-23 16:59:02 +0530

    first testing frequently

---
+++ src/ui/HomeBanner.tsx
@@ -0,0 +1,23 @@
+import { homeBanner } from "../assets";
+import Container from "./Container";
+import LinkButton from "./LinkButton";
+
+const HomeBanner = () => {
+  return (
+    <Container className="relative py-5 overflow-hidden">
+      <div className="relative">
+        <img
+          src="https://i.ibb.co/7GNTGcD/main-Homebanner.jpg"
+          alt="homeBanner"
+          className="w-full h-full object-cover rounded-md"
+        />
+        <div className="w-full h-full absolute top-0 left-0 bg-black/10" />
+      </div>
+      <div className="absolute inset-0 flex flex-col justify-center px-10">
+        <LinkButton className="w-44 flex items-center justify-center bg-whiteText text-darkText hover:bg-darkText hover:text-whiteText duration-200 mt-4" />
+      </div>
+    </Container>
+  );
+};
+
+export default HomeBanner;
